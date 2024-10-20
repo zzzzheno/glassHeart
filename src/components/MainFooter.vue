3b69181d2222da9dd877da059acb8b3f4bc0431f
zhen <allenwalker044@gmail.com>
2024-10-21 01:33:58 +0800

    add compoents：MainHeader & MainFooter add pages：PriacyPage

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex justify-center bg-brown-4"
+    style="box-shadow: 0px 6px 8px rgba(78, 52, 46, 0.1)"
+  >
+    <div class="flex justify-center q-py-lg" style="max-width: 1440px">
+      <a href="/">
+        <img
+          src="/src/assets/logo_white.svg"
+          alt="玻璃心選物"
+          style="width: 140px"
+        />
+      </a>
+      <div class="flex column">
+        <q-btn text-color="white" label="隱私權政策" unelevated to="Privacy">
+        </q-btn>
+        <q-btn text-color="white" label="購物須知" unelevated> </q-btn>
+        <q-btn text-color="white" label="退換貨方法" unelevated> </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "MainFooter",
+});
+</script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="fixed-top flex justify-center bg-white"
+    style="box-shadow: 0px 6px 8px rgba(78, 52, 46, 0.1)"
+  >
+    <q-toolbar class="q-py-lg justify-between" style="max-width: 1440px">
+      <a href="/">
+        <img
+          src="/src/assets/logo_brown.svg"
+          alt="玻璃心選物"
+          style="width: 100px"
+        />
+      </a>
+      <q-btn-dropdown
+        size="16px"
+        color="white"
+        text-color="brown-9"
+        label="餐廚"
+        unelevated
+      >
+        <q-list>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label class="text-body1 text-brown-9">碗盤</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </q-toolbar>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "MainHeader",
+});
+</script>
